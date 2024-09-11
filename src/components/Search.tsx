56fd2c9d83bf7fae2b4083d2ffe953ebaf755af5
Hamidreza Safarpour <hamidreza.safarpour01@gmail.com>
2024-09-11 19:15:53 +0330

    add weather component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import searchIcon from "../assets/icons/search.png";
 
 const Search = () => {
   return (
     <div className="input-box">
-      <input type="text" placeholder="Search City Name" />
+      <input type="text" placeholder="Search" />
       <button>
         <img src={searchIcon} alt="" />
       </button>
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import cloud from "../assets/icons/cloud.png";
+import rain from "../assets/icons/rain.png";
+import snow from "../assets/icons/snow.png";
+import sunCloud from "../assets/icons/sun-cloud.png";
+import sun from "../assets/icons/sun.png";
+import wind from "../assets/icons/wind.png";
+import waterDrop from "../assets/icons/water-drop.png";
 
 const Weather = () => {
-  return <div></div>;
+  return (
+    <div className="information">
+      <div className="temperature">
+        <span className="city-name">Tehran</span>
+        <img src={sun} alt="" />
+        <span className="temp">32°c</span>
+      </div>
+      <div className="wind-rain">
+        <div className="wind">
+          <img src={wind} alt="wind" />
+          <span>2.3 km/h</span>
+        </div>
+        <div className="rain">
+          <img src={waterDrop} alt="water drop" />
+          <span>3%</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
